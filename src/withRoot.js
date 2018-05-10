492c94bd1f228c326036960f9394c0d29aff9488
Paddy <paddy.sinclair.h@gmail.com>
2018-05-10 23:47:37 +0100

    innit

---
+++ src/withRoot.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { MuiThemeProvider } from 'material-ui/styles';
+import CssBaseline from 'material-ui/CssBaseline';
+import getPageContext from './getPageContext';
+// new
+import { ApolloProvider, getDataFromTree } from 'react-apollo';
+import initApollo from './initApollo';
+import Head from 'next/head';
+
+// Gets the display name of a JSX component for dev tools
+function getComponentDisplayName(Component) {
+  return Component.displayName || Component.name || 'Unknown';
+}
+
+export default ComposedComponent => {
+  return class WithRoot extends React.Component {
+    static displayName = `WithData(${getComponentDisplayName(ComposedComponent)})`;
+    static propTypes = {
+      serverState: PropTypes.object.isRequired,
+      pageContext: PropTypes.object
+    };
+    static async getInitialProps(ctx) {
+      // Initial serverState with apollo (empty)
+      let serverState;
+
+      // Evaluate the composed component's getInitialProps()
+      let composedInitialProps = {};
+      if (ComposedComponent.getInitialProps) {
+        composedInitialProps = await ComposedComponent.getInitialProps(ctx);
+      }
+
+      // Run all GraphQL queries in the component tree
+      // and extract the resulting data
+      const apollo = initApollo();
+      try {
+        // create the url prop which is passed to every page
+        const url = {
+          query: ctx.query,
+          asPath: ctx.asPath,
+          pathname: ctx.pathname
+        };
+
+        // Run all GraphQL queries
+        await getDataFromTree(<ComposedComponent ctx={ctx} url={url} {...composedInitialProps} />, {
+          router: {
+            asPath: ctx.asPath,
+            pathname: ctx.pathname,
+            query: ctx.query
+          },
+          client: apollo
+        });
+      } catch (error) {
+        // Prevent Apollo Client GraphQL errors from crashing SSR.
+        // Handle them in components via the data.error prop:
+        // http://dev.apollodata.com/react/api-queries.html#graphql-query-data-error
+      }
+
+      if (!process.browser) {
+        // getDataFromTree does not call componentWillUnmount
+        // head side effect therefore need to be cleared manually
+        Head.rewind();
+      }
+
+      // Extract query data from the Apollo store
+      serverState = {
+        apollo: {
+          data: apollo.cache.extract()
+        }
+      };
+
+      return {
+        serverState,
+        ...composedInitialProps
+      };
+    }
+
+    constructor(props, context) {
+      super(props, context);
+      this.apollo = initApollo(this.props.serverState.apollo.data);
+      this.pageContext = this.props.pageContext || getPageContext();
+    }
+
+    componentDidMount() {
+      // Remove the server-side injected CSS.
+      const jssStyles = document.querySelector('#jss-server-side');
+      if (jssStyles && jssStyles.parentNode) {
+        jssStyles.parentNode.removeChild(jssStyles);
+      }
+    }
+
+    pageContext = null;
+
+    render() {
+      // MuiThemeProvider makes the theme available down the React tree thanks to React context.
+      return (
+        <ApolloProvider client={this.apollo}>
+          <MuiThemeProvider theme={this.pageContext.theme} sheetsManager={this.pageContext.sheetsManager}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <ComposedComponent {...this.props} />
+          </MuiThemeProvider>
+        </ApolloProvider>
+      );
+    }
+  };
+};
