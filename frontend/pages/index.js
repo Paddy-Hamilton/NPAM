1a64e4aff3bdbee4defb95c8ea061d9cb251b0c3
Paddy Hamilton <patrick.hamilton@croningroupplc.com>
2018-05-22 13:31:27 +0100

    Only show create post id signed in

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import withData from '../lib/withData';
 import PageLayout from '../components/PageLayout';
-import ArticleGrid from '../components/ArticleGrid';
 import GetPostQuery from '../components/GetPostQuery';
+import ArticleGrid from '../components/ArticleGrid';
 import CreatePostBtn from '../components/CreatePost';
 import CreatePostModal from '../components/CreatePostModal';
-
+import { CURRENT_USER } from '../graphql/queries.graphql';
+import { Query } from 'react-apollo';
 class Index extends React.Component {
   render() {
     return (
       <PageLayout>
         <GetPostQuery render={posts => <ArticleGrid posts={posts} />} />
-        <CreatePostBtn />
-        <CreatePostModal />
+        <Query query={CURRENT_USER}>
+          {({ loading, error, data: { me } }) => {
+            if (loading || !me) return null;
+            if (error) return console.error(error);
+            return (
+              <React.Fragment>
+                <CreatePostBtn />
+                <CreatePostModal />
+              </React.Fragment>
+            );
+          }}
+        </Query>
       </PageLayout>
     );
   }
