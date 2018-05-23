0413588f6b8b39040d651db5c6f1c46cda9cded6
Paddy <paddy.sinclair.h@gmail.com>
2018-05-23 07:37:42 +0100

    Card layout update

--- frontend/components/ArticleGrid/index.jsx
+++ frontend/components/ArticleGrid/index.jsx
@@ -3,10 +3,15 @@ import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import Link from 'next/link';
 import Grow from '@material-ui/core/Grow';
 import moment from 'moment';
+
 const styles = theme => ({
   root: {
     padding: '0 12px',
@@ -33,6 +38,10 @@ const styles = theme => ({
     color: theme.palette.primary.dark,
     textDecoration: 'none',
     transition: 'color 0.3s ease-in-out'
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%' // 16:9
   },
   date: {
     color: theme.palette.primary.light,
@@ -79,14 +88,28 @@ class ArticleGrid extends Component {
               <Grow in timeout={incrimentTimeout()} key={post.id || post.title}>
                 <Grid item xs={12} md={6} lg={4}>
                   <Card className={classes.post}>
+                    <CardMedia className={classes.media} image={post.img} title="Post image" />
                     <CardContent>
-                      <div>
-                        <h3>{post.title}</h3>
-                        <p className={classes.date}>
-                          <small>{moment(post.createdAt).format('MMMM Do YYYY')}</small>
-                        </p>
-                      </div>
+                      <Typography gutterBottom variant="headline" component="h2">
+                        {post.title}
+                      </Typography>
+                      <Typography component="p" gutterBottom>
+                        {post.text}
+                      </Typography>
+                      <Typography >{post.author.name}</Typography>
+                      <Typography variant="caption">
+                        <small>{moment(post.createdAt).format('MMMM Do YYYY')}</small>
+                      </Typography>
                     </CardContent>
+
+                    <CardActions>
+                      <Button size="small" color="primary">
+                        View
+                      </Button>
+                      <Button size="small" color="primary">
+                        Share
+                      </Button>
+                    </CardActions>
                   </Card>
                 </Grid>
               </Grow>
